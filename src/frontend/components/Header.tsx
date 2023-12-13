668c4ae4586cd2dc0bc9c33280c236aaa0619dae
SEAMUS <snaaykens@gmail.com>
2023-12-13 15:52:00 -0700

    created initial header component strcuture

---
+++ src/frontend/components/Header.tsx
@@ -0,0 +1,18 @@
+import {
+  Box,
+  Grid,
+  Heading
+} from '@chakra-ui/react'
+
+
+export const Header = () => {
+  return (
+    <div>
+      <Grid>
+        <Box>
+          <Heading>FeatherDiary</Heading>
+        </Box>
+      </Grid>
+    </div>
+  )
+}
